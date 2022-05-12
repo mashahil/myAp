5949cafd7a0c26ce870fec820a37c506b4006724
Muhammed Mashahil P <mashahil@yahoo.com>
2022-05-12 16:55:37 +0530

    initial2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,5 +8,7 @@
 </head>
 <body>
     <h1>hi</h1>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus esse saepe illo dolores repudiandae debitis magni omnis harum doloremque dicta reprehenderit, deserunt nihil tempore accusantium commodi suscipit veniam aperiam eaque.
+</P>
 </body>
 </html>
